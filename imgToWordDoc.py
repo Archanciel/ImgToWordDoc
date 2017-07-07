e9d31094bb718785d394d359834ea10f4c871e8e
Archanciel <jp.schnyder@gmail.com>
2017-07-07 20:03:07 +0200

    fixed bug file name sort

--- imgToWordDoc.py
+++ imgToWordDoc.py
@@ -4,6 +4,7 @@ from docx import Document
 from docx.shared import Inches
 from docx.shared import Cm
 from PIL import Image
+import re
 
 IMG_WIDTH = 19.5
 MARGIN = 1
@@ -19,7 +20,6 @@ def createWordDocWithImgInDir():
 
 	fileLst = os.listdir(curDir)
 	imgFileLst = list(filter(lambda name: ".jpg" in name,fileLst))
-	imgFileLst.sort()
 
 	doc = Document()
 
@@ -42,6 +42,21 @@ def createWordDocWithImgInDir():
 
 	doc.save(fullTargetFileName)
 	print("{0} file created with {1} image(s)".format(fullTargetFileName,i))
+
+
+	'''
+	Using this function, a list of file names containing 1.jpg, 11.jpg, 2.jpg will 
+	be ordered so: 1.jpg, 2.jpg, 11.jpg !
+	:param fileName: 
+	:return: 
+	'''
+	m = re.search(r'^(\d+).*', fileName)
+
+	if m == None:
+		raise NameError("Invalid img file name encountered: {0}. Img file names must start with a number for them to be inserted in the right order !".format(fileName))
+
+	return int(m.group(1))
+
 
 def setDocMargins(doc):
 	sections = doc.sections
@@ -69,4 +84,7 @@ def determineUniqueFileName(targetWordFileName, targetWordFileExt):
 
 
 if __name__ == '__main__':
-	createWordDocWithImgInDir()
+	try:
+		createWordDocWithImgInDir()
+	except NameError as e:
+		print(e)
