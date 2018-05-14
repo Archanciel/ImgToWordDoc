1b4cb7fea29fd45e25b822656a13eb09bee5e2ce
Jean-Pierre Schnyder <jp.schnyder@gmail.com>
2018-05-14 20:28:19 +0200

    improved and extended help

--- imgToWordDoc.py
+++ imgToWordDoc.py
@@ -30,25 +30,36 @@ def getCommandLineArgs(argList):
     :return: document name (may be None) and insertion point
     '''
     parser = argparse.ArgumentParser(
-        description="Add all images contained in current dir to a Word document. Each image " \
+        description="Adds or inserts all or part of the images contained in the current dir to a Word document. Each image " \
                     "is added in a new paragraph. To facilitate further edition, the image " \
-                    "is preceded by a text line and followed by a bullet point section. " \
-                    "The images are added according to order of the number contained in their " \
+                    "is preceded by a header line and followed by a bullet point section. " \
+                    "The images are added according to the ascending order of the number contained in their " \
                     "file name. An error will occur if one of the image file name does not "
-                    "contain a number (valid image file names: 1.jpg, image2.jpg, 3.png, ...). " \
+                    "contain a number (valid image file names are: 1.jpg, image2.jpg, 3.png, ...). " \
                     "If no document name is specified, the created document has " \
                     "the same name as the containing dir. An existing document with " \
                     "same name is never overwritten. Instead, a new document with a " \
                     "name incremented by 1 (i.e. myDoc1.docx, myDoc2.docx, ...) " \
-                    "is created.")
-    parser.add_argument("-d", "--document", nargs="?", help="existing document to which the current dir images are " \
-                                                            "to be added. For your convinience, the initial document is " \
+                    "is created. " \
+                    "Using the utility in add mode, i.e. without specifying an insertion " \
+                    "point, creates a new document in which the specified images will be added. " \
+                    "If the current dir already contains a document with images and comments you " \
+                    "want to keep, use the insertion parameter which will insert the new images at " \
+                    "the specified position and preserve the initial content. ")
+    parser.add_argument("-d", "--document", nargs="?", help="existing document to which the images are " \
+                                                            "to be added. For your convenience, the initial document is " \
                                                             "not modified. Instead, the original document is copied with a " \
-                                                            "name incremented by one and the images are added to the copy.")
+                                                            "name incremented by one and the images are added/inserted to the copy.")
     parser.add_argument("-i", "--insertionPos", type=int, nargs="?",
                         help="paragraph number BEFORE which to insert the " \
                              "images. 1 --> start of document (before paragraph 1). " \
                              "0 --> end of document. ")
+    parser.add_argument("-p", "--pictures", nargs="*", help="numbers contained in the image file names which are selected " \
+                                                            "to be inserted in the existing document. Exemple: -p 1 8 4-6 9-10 " \
+                                                            "means the images whose name contain the specified numbers will be added or " \
+                                                            "or inserted in ascending number order, in this case 1, 4, 5, 6, 8, 9, 10. " \
+                                                            "If this parm is omitted, all the pictures in the curreent " \
+                                                            "dir are added or inserted. ")
     args = parser.parse_args(argList)
 
     return args.document, args.insertionPos
